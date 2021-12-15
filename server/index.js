50ff7a52e201152a5438da92f9c98b99b3ed351c
Carlotta Chen <carlottachen92@gmail.com>
2021-12-15 13:21:10 -0800

    Second attempt to make CSS work

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, '../index.html'));
+app.get('/js', function (req, res) {
+	res.sendFile(path.join(__dirname, '../index.js'));
 })
 const port = process.env.PORT || 4030;
 app.listen(port, () => { console.log(`Working on PORT ${port}`) });
