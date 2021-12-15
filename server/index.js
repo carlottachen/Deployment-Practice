456d3de16fb3fb0633b6beb7ebefddf173cd75de
Carlotta Chen <carlottachen92@gmail.com>
2021-12-15 14:07:12 -0800

    Final fixes for css file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//create endpoint to each file
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../index.html'));
 })
