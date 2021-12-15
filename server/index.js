6db694649b5dccdfa16b977ea6f9a2a01eae0071
Carlotta Chen <carlottachen92@gmail.com>
2021-12-15 15:25:49 -0800

    Add link to heroku hosted site

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../index.html'));
 })
 app.get('/js', (req, res) => {
-	res.sendFile(path.join(__dirname, '../main.js'))
+	res.sendFile(path.join(__dirname, '../main.js'));
 })
 app.get('/css', (req, res) => {
-	res.sendFile(path.join(__dirname, '../style.css'))
+	res.sendFile(path.join(__dirname, '../style.css'));
 })
 const port = process.env.PORT || 4030;
 app.listen(port, () => { console.log(`Working on PORT ${port}`) });
