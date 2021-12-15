44355f1ed33928ec479ee9e2e3702c03942a4298
Carlotta Chen <carlottachen92@gmail.com>
2021-12-15 13:51:37 -0800

    6th attempt

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-/*
+
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../index.html'));
-})*/
+})
 app.get('/js', (req, res) => {
-	res.sendFile(path.join(__dirname, './server/index.js'));
+	res.sendFile(path.join(__dirname, '../main.js'))
 })
 const port = process.env.PORT || 4030;
 app.listen(port, () => { console.log(`Working on PORT ${port}`) });
